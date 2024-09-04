9df9bbf00da3bfce6c1050bd0783e0fa3e84cd3e
adgodly <2240389397@qq.com>
2024-09-04 20:10:39 +0800

    lab2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,15 +1,39 @@
 #include "tcp_receiver.hh"
+#include "wrapping_integers.hh"
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
   // Your code here.
-  (void)message;
+  if ( writer().has_error() ) {
+    return;
+  }
+  if ( message.RST ) {
+    reader().set_error();
+    return;
+  }
+  if ( not zero_point_.has_value() ) {
+    if ( !message.SYN ) {
+      return;
+    }
+    zero_point_.emplace( message.seqno );
+  }
+  uint64_t checkpoint = writer().bytes_pushed() + 1; // SYN Flag
+  uint64_t absolute_seqno = message.seqno.unwrap( zero_point_.value(), checkpoint );
+  uint64_t stream_index = absolute_seqno + static_cast<uint64_t>( message.SYN ) - 1;
+
+  reassembler_.insert( stream_index, message.payload, message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
-  return {};
+  uint64_t capacity = reassembler_.writer().available_capacity();
+  uint16_t window_size = capacity > UINT16_MAX ? UINT16_MAX : static_cast<uint16_t>( capacity );
+  if ( zero_point_.has_value() ) {
+    uint64_t ackno = writer().bytes_pushed() + 1 + static_cast<uint64_t>( writer().is_closed() );
+    return { Wrap32::wrap( ackno, zero_point_.value() ), window_size, writer().has_error() };
+  }
+  return { nullopt, window_size, writer().has_error() };
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,15 +5,21 @@ using namespace std;
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+
+  return Wrap32( static_cast<uint32_t>( n ) + zero_point.raw_value_ );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  const uint64_t n_low32 { this->raw_value_ - zero_point.raw_value_ };
+  const uint64_t c_low32 { checkpoint & MASK_LOW_32 };
+  const uint64_t res { ( checkpoint & MASK_HIGH_32 ) | n_low32 };
+  if ( res >= BASE and n_low32 > c_low32 and ( n_low32 - c_low32 ) > ( BASE / 2 ) ) {
+    return res - BASE;
+  }
+  if ( res < MASK_HIGH_32 and c_low32 > n_low32 and ( c_low32 - n_low32 ) > ( BASE / 2 ) ) {
+    return res + BASE;
+  }
+  return res;
 }
